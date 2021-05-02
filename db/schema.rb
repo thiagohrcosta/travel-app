00878921c786b818369e040ea946a5d57116c33b
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-02 12:21:32 -0300

    Adds controler to create, edit and delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_010903) do
+ActiveRecord::Schema.define(version: 2021_05_02_151814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_02_010903) do
     t.string "photo"
     t.string "description"
     t.string "city"
-    t.string "country"
+    t.string "place"
     t.string "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
